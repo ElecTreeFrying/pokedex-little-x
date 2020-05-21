44e184d37d27eecae5c28101a18ead4310ce32e3
John James Ermitano <johnjamesermitano@yahoo.com>
2020-05-22 00:04:43 +0800

    adjusted dashed pokemon names and complex pokemon names to normal

--- src/app/_common/pipes/card-text.pipe.ts
+++ src/app/_common/pipes/card-text.pipe.ts
@@ -7,11 +7,28 @@ import { capitalize } from 'lodash';
 })
 export class CardTextPipe implements PipeTransform {
 
+  names = [
+    { name: 'Nidoran♀', key: 'nidoran-f' },
+    { name: 'Nidoran♂', key: 'nidoran-m' },
+    { name: 'Mr. Mime', key: 'mr-mime' },
+    { name: 'Mime Jr.', key: 'mime-jr' },
+    { name: 'Ho-Oh', key: 'ho-oh' },
+    { name: 'Jangmo-o', key: 'jangmo-o' },
+    { name: 'Hakamo-o', key: 'hakamo-o' },
+    { name: 'Kommo-o', key: 'kommo-o' },
+    { name: 'Oricorio Pom-Pom', key: 'oricorio-pom-pom' },
+    { name: 'Kommo-o Totem', key: 'kommo-o-totem' },
+  ];
+
   transform(value: string, ...args: unknown[]): string {
 
     if (!value) return;
 
-    return capitalize(value);
+    if (!this.names.find(e => e.key === value)) {
+      return value.split('-').map(e => capitalize(e)).join(' ');
+    } else {
+      return this.names.find(e => e.key === value).name;
+    }
   }
 
 }
