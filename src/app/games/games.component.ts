3712c997ccd24eb37a9fa05e900779b67dfb5b4f
John James Ermitano <johnjamesermitano@yahoo.com>
2020-05-25 19:47:11 +0800

    fixed all pokemon and national pokedex entry numbers in pokemon cards

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -144,6 +144,7 @@ export class GamesComponent implements OnInit, AfterViewInit, OnDestroy {
     const isItems = route === 'items';
     const isCategory = route === 'category';
 
+    this.route['entry_sort_id'] = res.id === 0 || res.id === 1 || res.id === 11;
     this.route['isGames'] = isGames;
     this.route['isItems'] = isItems;
     this.route['isCategory'] = isCategory;
