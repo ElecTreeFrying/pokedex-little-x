f3e30f1e1e69e243378f987211b2e971bd8dbaa1
John James Ermitano <johnjamesermitano@yahoo.com>
2020-05-26 11:58:28 +0800

    bump changes

--- src/app/_common/pipes/id-to-image.pipe.ts
+++ src/app/_common/pipes/id-to-image.pipe.ts
@@ -16,11 +16,11 @@ export class IdToImagePipe implements PipeTransform {
     
     if (!value) return;
 
-    if (type === 'byte64') {
+    else if (type === 'byte64') {
       return `data:image/png;base64,${value.replace('data:image/png;base64,', '')}`;
     } 
 
-    if (type === 'conquest-gallery') {
+    else if (type === 'conquest-gallery') {
       return `https://res.cloudinary.com/electreefrying/image/upload/v1590416403/pokedex-little/conquest_library/${value.filename}`;
     } 
     
